4c94187e46501dd9ecfa75ba829bbe9ba723159f
info@pablolujan.com <info@pablolujan.com>
2024-04-15 23:01:30 +0000

    Automatic commit on Mon Apr 15 23:01:30 UTC 2024, new version of the site was generated

--- reload_site.sh
+++ reload_site.sh
@@ -4,4 +4,4 @@ current_datetime=$(date)
 commit_message="Automatic commit on $current_datetime, new version of the site was generated"
 git add .
 git commit -m "$commit_message"
-git push origin main
+git push -u origin react-bricks

4bf0ffde1ed4c4c0ed8831e288532d84a527260e
pablo-lujan <62119503+pablo-lujan@users.noreply.github.com>
2024-04-17 03:08:48 +0000

    Automatic commit on Wed Apr 17 03:08:48 UTC 2024, new version of the site was generated

--- reload_site.sh
+++ reload_site.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/environment/site/web-builder
+git pull -u origin react-bricks
 current_datetime=$(date)
 commit_message="Automatic commit on $current_datetime, new version of the site was generated"
 git add .
